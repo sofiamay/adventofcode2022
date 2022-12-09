37ed534e1eadba372be62a656ae276c723cd9662
Crutcher Dunnavant <crutcher@gmail.com>
2022-12-08 19:35:34 -0800

    TreeBuilder

--- src/day07.ts
+++ src/day07.ts
@@ -66,6 +66,24 @@ export class DirNode extends TreeNode {
     }
   }
 
+  ensureDir(path: string[]): DirNode {
+    let current: DirNode = this;
+    for (const p of path) {
+      let child = current.entries.get(p);
+      if (child == undefined) {
+        child = new DirNode();
+        current.entries.set(p, child);
+      }
+
+      if (child instanceof DirNode) {
+        current = child;
+      } else {
+        throw `${p} is a file not a directory`;
+      }
+    }
+    return current;
+  }
+
   format(indent: number = 0): string {
     let parts: string[] = ["/"];
     for (const [name, node] of this.entries) {
@@ -75,6 +93,80 @@ export class DirNode extends TreeNode {
     }
     return parts.join("\n");
   }
+}
+
+export class PWD {
+  path: string[];
+
+  constructor(path: string[] = []) {
+    this.path = [...path];
+  }
+
+  toString(): string {
+    let result = this.path.join("/");
+    return "/" + result;
+  }
+
+  watch(commandLog: CommandLog): void {
+    if (!commandLog.command.startsWith("$ cd")) return;
+
+    const cmd = commandLog.command.split(/\s+/);
+    if (cmd.length < 2) {
+      console.log("'$ cd' command formatted incorrectly");
+      return;
+    }
+    let arg = cmd[2];
+    if (arg == "/") {
+      this.path = [];
+    } else if (arg == "..") {
+      this.path.pop();
+    } else {
+      this.path.push(arg);
+    }
+  }
+}
+
+export class TreeBuilder {
+  root: DirNode;
+  pwd: PWD;
+
+  constructor() {
+    this.root = new DirNode();
+    this.pwd = new PWD();
+  }
+
+  watch(cmdLog: CommandLog): void {
+    this.pwd.watch(cmdLog);
+    let dir = this.root.ensureDir(this.pwd.path);
+
+    // Is ls command:
+    if (cmdLog.command.split(/\s+/)[1] == "ls") {
+      for (const [name, node] of parseLS(cmdLog.results)) {
+        if (!dir.entries.has(name)) {
+          dir.entries.set(name, node);
+        }
+      }
+    }
+  }
+}
+
+export function parseLS(ls: string[]): Map<string, TreeNode> {
+  let results: Map<string, TreeNode> = new Map<string, TreeNode>();
+
+  ls.forEach((line) => {
+    let words = line.split(/\s+/);
+    // is directory
+    if (words[0] == "dir") {
+      let dirName = words[1];
+      results.set(dirName, new DirNode());
+    } else {
+      // is file
+      let fileSize = Number(words[0]);
+      let fileName = words[1];
+      results.set(fileName, new FileNode(fileSize));
+    }
+  });
+  return results;
 }
 
 export default {
--- tests/day07.spec.ts
+++ tests/day07.spec.ts
@@ -3,7 +3,15 @@
 import "mocha";
 import { assert, expect } from "chai";
 
-import day07, { CommandLog, FileNode, DirNode, TreeNode } from "../src/day07";
+import day07, {
+  CommandLog,
+  FileNode,
+  DirNode,
+  TreeNode,
+  PWD,
+  parseLS,
+  TreeBuilder,
+} from "../src/day07";
 
 const exampleInput = `
 $ cd /
@@ -30,6 +38,75 @@ $ ls
 5626152 d.ext
 7214296 k
 `;
+
+describe("Command Parser", () => {
+  it("chunk input", () => {
+    expect(day07.parseCommandLogs(exampleInput)).to.eql([
+      new CommandLog("$ cd /"),
+      new CommandLog("$ ls", [
+        "dir a",
+        "14848514 b.txt",
+        "8504156 c.dat",
+        "dir d",
+      ]),
+      new CommandLog("$ cd a"),
+      new CommandLog("$ ls", ["dir e", "29116 f", "2557 g", "62596 h.lst"]),
+      new CommandLog("$ cd e"),
+      new CommandLog("$ ls", ["584 i"]),
+      new CommandLog("$ cd .."),
+      new CommandLog("$ cd .."),
+      new CommandLog("$ cd d"),
+      new CommandLog("$ ls", [
+        "4060174 j",
+        "8033020 d.log",
+        "5626152 d.ext",
+        "7214296 k",
+      ]),
+    ]);
+  });
+});
+
+describe("parseLS", () => {
+  it("parses lines of text into dirs and files", () => {
+    const ls = ["dir a", "14848514 b.txt", "8504156 c.dat"];
+
+    expect(parseLS(ls)).to.eql(
+      new Map<string, TreeNode>([
+        ["a", new DirNode()],
+        ["b.txt", new FileNode(14848514)],
+        ["c.dat", new FileNode(8504156)],
+      ])
+    );
+  });
+});
+
+describe("PWD", () => {
+  it("constructor", () => {
+    let pwd = new PWD();
+    expect(pwd.path).to.eql([]);
+  });
+  it("state transition", () => {
+    let pwd = new PWD();
+    pwd.watch(new CommandLog("$ cd /"));
+    expect(pwd.path).to.eql([]);
+
+    pwd.watch(new CommandLog("$ cd foo"));
+    pwd.watch(new CommandLog("$ cd bar"));
+    expect(pwd.path).to.eql(["foo", "bar"]);
+
+    pwd.watch(new CommandLog("$ cd .."));
+    expect(pwd.path).to.eql(["foo"]);
+
+    pwd.watch(new CommandLog("$ ls"));
+    expect(pwd.path).to.eql(["foo"]);
+
+    pwd.watch(new CommandLog("$ cd /"));
+    expect(pwd.path).to.eql([]);
+
+    pwd.watch(new CommandLog("$ cd .."));
+    expect(pwd.path).to.eql([]);
+  });
+});
 
 describe("DirTree", () => {
   it("FileNode formatting", () => {
@@ -72,33 +149,32 @@ describe("DirTree", () => {
   });
 });
 
-describe("Day 7", () => {
-  it("some basic test", () => {
-    assert.equal(2, 2);
-  });
+describe("TreeBuilder", () => {
+  it("DirNode formatting", () => {
+    let commandLogs = day07.parseCommandLogs(exampleInput);
+    let tree_builder = new TreeBuilder();
+    commandLogs.forEach((commandLog: CommandLog) => {
+      tree_builder.watch(commandLog);
+    });
 
-  it("chunk input", () => {
-    expect(day07.parseCommandLogs(exampleInput)).to.eql([
-      new CommandLog("$ cd /"),
-      new CommandLog("$ ls", [
-        "dir a",
-        "14848514 b.txt",
-        "8504156 c.dat",
-        "dir d",
-      ]),
-      new CommandLog("$ cd a"),
-      new CommandLog("$ ls", ["dir e", "29116 f", "2557 g", "62596 h.lst"]),
-      new CommandLog("$ cd e"),
-      new CommandLog("$ ls", ["584 i"]),
-      new CommandLog("$ cd .."),
-      new CommandLog("$ cd .."),
-      new CommandLog("$ cd d"),
-      new CommandLog("$ ls", [
-        "4060174 j",
-        "8033020 d.log",
-        "5626152 d.ext",
-        "7214296 k",
-      ]),
-    ]);
+    let tree = tree_builder.root;
+    expect(tree.format()).to.equal(
+      [
+        "/",
+        "  a /",
+        "      e /",
+        "          i File(size: 584)",
+        "      f File(size: 29116)",
+        "      g File(size: 2557)",
+        "      h.lst File(size: 62596)",
+        "  b.txt File(size: 14848514)",
+        "  c.dat File(size: 8504156)",
+        "  d /",
+        "      j File(size: 4060174)",
+        "      d.log File(size: 8033020)",
+        "      d.ext File(size: 5626152)",
+        "      k File(size: 7214296)",
+      ].join("\n")
+    );
   });
 });
